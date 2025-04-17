108ec57f6c9f9e3e57c16144dceab77af6ddb10e
erkelost <1256029807@qq.com>
2025-04-17 09:05:11 +0800

    chore: update next version

---
+++ next.config.ts
@@ -0,0 +1,28 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  reactStrictMode: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
