42a8454f4437e1e2937e16c1e3ca5da1847a4650
erkelost <1256029807@qq.com>
2025-05-22 10:46:40 +0800

    chore: update templates

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to the Application</h1>
-      <p>This is the default page.</p>
     </div>
   );
 }
